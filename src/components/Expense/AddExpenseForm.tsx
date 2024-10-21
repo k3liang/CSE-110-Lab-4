ec24b4331d2b60fcdd58ca08423ba6f23809bac0
k3liang <130005349+k3liang@users.noreply.github.com>
2024-10-21 14:46:55 -0700

    Exercise 1 Done

--- src/components/Expense/AddExpenseForm.tsx
+++ src/components/Expense/AddExpenseForm.tsx
@@ -1,48 +1,78 @@
-import React, { useState } from "react";
+import React, {
+    useState,
+    useContext,
+    ReactElement,
+    ReactHTMLElement,
+} from "react";
+import { AppContext } from "../../context/AppContext";
+import { Expense } from "../../types/types";
 const AddExpenseForm = () => {
-  // Exercise: Consume the AppContext here
+    // Exercise: Consume the AppContext here
+    const { expenses, setExpenses } = useContext(AppContext);
 
-  // Exercise: Create name and cost to state variables
+    // Exercise: Create name and cost to state variables
+    const [name, setName] = useState("");
+    const [cost, setCost] = useState(0);
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
 
-    // Exercise: Add add new expense to expenses context array
-  };
+        // Exercise: Add add new expense to expenses context array
+        // const newExpense : Expense = { id: "string", name: name, cost: cost};
 
-  return (
-    <form onSubmit={(event) => onSubmit(event)}>
-      <div className="row">
-        <div className="col-sm">
-          <label htmlFor="name">Name</label>
-          <input
-            required
-            type="text"
-            className="form-control"
-            id="name"
-            value={""}
-            // HINT: onChange={}
-          ></input>
-        </div>
-        <div className="col-sm">
-          <label htmlFor="cost">Cost</label>
-          <input
-            required
-            type="text"
-            className="form-control"
-            id="cost"
-            value={0}
-            // HINT: onChange={}
-          ></input>
-        </div>
-        <div className="col-sm">
-          <button type="submit" className="btn btn-primary mt-3">
-            Save
-          </button>
-        </div>
-      </div>
-    </form>
-  );
+        setExpenses((prevExpenses) =>
+            prevExpenses.length > 0
+                ? prevExpenses.concat([
+                      {
+                          id: prevExpenses[prevExpenses.length - 1].id + 1 + "",
+                          name: name,
+                          cost: cost,
+                      },
+                  ])
+                : prevExpenses.concat([{ id: 0 + "", name: name, cost: cost }])
+        );
+    };
+
+    const updateCost = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (!isNaN(+e.target.value))
+            setCost(+e.target.value);
+    };
+
+    return (
+        <form onSubmit={(event) => onSubmit(event)}>
+            <div className="row">
+                <div className="col-sm">
+                    <label htmlFor="name">Name</label>
+                    <input
+                        required
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        value={name}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                            setName(e.target.value);
+                        }}
+                    ></input>
+                </div>
+                <div className="col-sm">
+                    <label htmlFor="cost">Cost</label>
+                    <input
+                        required
+                        type="text"
+                        className="form-control"
+                        id="cost"
+                        value={cost}
+                        onChange={updateCost}
+                    ></input>
+                </div>
+                <div className="col-sm">
+                    <button type="submit" className="btn btn-primary mt-3">
+                        Save
+                    </button>
+                </div>
+            </div>
+        </form>
+    );
 };
 
 export default AddExpenseForm;
